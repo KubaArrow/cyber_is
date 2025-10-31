dc298e71113feddfe972d7dbc3aee25b0c0b11af
kuba_arrow <jakubstrzalkaa@gmail.com>
2025-10-31 15:01:20 +0100

    fix

--- cyber_is_bringup/bash/setup_robot.sh
+++ cyber_is_bringup/bash/setup_robot.sh
@@ -21,7 +21,7 @@ exec ros2 launch cyber_is_bringup is_bringup.launch.py \
   use_gui:=false \
   start_navigation:=true \
   autostart:=true \
-  map:=/home/is/cyber_is_ws/src/cyber_is/maps/map.yaml \
+  map:=/home/is/cyber_ws/src/cyber_is/maps/map.yaml \
   start_uart_bridge:=true \
   start_led_controller:=true \
   start_supervisor:=true \
