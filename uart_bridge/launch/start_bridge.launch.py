afeeeedc3542c7c6efdeed87e4159e837efe6600
kuba_arrow <jakubstrzalkaa@gmail.com>
2025-10-31 01:38:59 +0100

    fix

--- uart_bridge/launch/start_bridge.launch.py
+++ uart_bridge/launch/start_bridge.launch.py
@@ -1,24 +1,16 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
-    uart_port = LaunchConfiguration('uart_port')
-    baud_rate = LaunchConfiguration('baud_rate')
-
     return LaunchDescription([
-        DeclareLaunchArgument('uart_port', default_value='/dev/ttyUSB0'),
-        DeclareLaunchArgument('baud_rate', default_value='115200'),
         Node(
             package='uart_bridge',
             executable='uart_bridge',
             name='uart_bridge',
             output='screen',
             parameters=[
-                {'uart_port': uart_port},
-                {'baud_rate': baud_rate},
+                {'uart_port': '/dev/ttyACM0'},
                 {'frequency': 200},
                 {'twist_topic': '/cmd_vel'},
                 {'pose_topic': '/slam_out_pose'},
@@ -34,3 +26,4 @@ def generate_launch_description() -> LaunchDescription:
             ],
         )
     ])
+
